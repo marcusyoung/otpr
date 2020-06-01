71a3222e150fb329e42965077851f465917a3c3c
Marcus Young <m.a.young@soton.ac.uk>
2020-06-01 18:57:30 +0100

    Update Memory to JVM

--- tests/launch-otp.R
+++ tests/launch-otp.R
@@ -2,6 +2,6 @@
 # Requires: Sys.setenv("OTP_ON_LOCALHOST" = TRUE)
 
 if(identical(Sys.getenv("OTP_ON_LOCALHOST"), "TRUE")) {
-system("java -Xmx6G -jar /otp/otp.jar --router otpr-test --graphs /otp/graphs --server", intern = FALSE, wait = FALSE)
+system("java -Xmx2G -jar /otp/otp.jar --router otpr-test --graphs /otp/graphs --server", intern = FALSE, wait = FALSE)
 Sys.sleep(5)
 }
