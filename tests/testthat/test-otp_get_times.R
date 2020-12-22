d81c92a48ed61952e91c9e31eb01c3390eb891fa
Marcus Young <m.a.young@soton.ac.uk>
2020-12-22 20:08:55 +0000

    Add tests for OTPv2

--- tests/testthat/test-otp_get_times.R
+++ tests/testthat/test-otp_get_times.R
@@ -2,16 +2,18 @@ context("Test the otp_get_times function")
 
 if (identical(Sys.getenv("OTP_ON_LOCALHOST"), "TRUE")) {
   otpcon <- otp_connect()
+  otpcon_v2 <- otp_connect(port = 9190)
   
   # setup test query results - amend for a new graph build
   fromPlace <- c(50.75840, -1.30291)
   toPlace <- c(50.59871, -1.19133)
-  transit_duration <- 94.32
+  transit_duration <- 94.3
   date <- "06-01-2020"
   time <- "12:00:00"
   legs_number <- 5
-  arriveBy_time <- as.POSIXct("2020-06-01 11:52:50 BST")
-  response_query <- paste("http://localhost:8080/otp/routers/default/plan?fromPlace=", paste(fromPlace, collapse = ","), "&toPlace=", paste(toPlace, collapse = ","), "&mode=TRANSIT,WALK&date=", date, "&time=", time, "&maxWalkDistance=800&walkReluctance=2&waitReluctance=1&arriveBy=FALSE&transferPenalty=0&minTransferTime=600", sep="")
+  arriveBy_time <- as.POSIXct("2020-06-01 11:53:00 BST")
+  response_query <- paste("http://localhost:8080/otp/routers/default/plan?fromPlace=", paste(fromPlace, collapse = ","), "&toPlace=", paste(toPlace, collapse = ","), "&mode=TRANSIT,WALK&date=", date, "&time=", time, "&maxWalkDistance=1600&walkReluctance=4&waitReluctance=2&arriveBy=TRUE&transferPenalty=10&minTransferTime=600&optimize=TRANSFERS", sep="")
+  response_query_otp2 <- paste("http://localhost:9190/otp/routers/default/plan?fromPlace=", paste(fromPlace, collapse = ","), "&toPlace=", paste(toPlace, collapse = ","), "&mode=TRANSIT,WALK&date=", date, "&time=", time, "&maxWalkDistance=1600&walkReluctance=4&waitReluctance=2&arriveBy=TRUE&transferPenalty=10&minTransferTime=600&optimize=TRANSFERS", sep="")
 }
 
 skip_if_no_otp <- function() {
@@ -19,7 +21,9 @@ skip_if_no_otp <- function() {
     skip("Not running test as the environment variable OTP_ON_LOCALHOST is not set to TRUE")
 }
 
-test_that("Check for invalid mode", {
+# OTP1
+
+test_that("OTP1 Check for invalid mode", {
   skip_if_no_otp()
   error <-
     try(otp_get_times(otpcon,
@@ -30,7 +34,7 @@ test_that("Check for invalid mode", {
   expect_equal(grepl("Mode must be one of:", error, fixed = TRUE), TRUE)
 })
 
-test_that("basic query no detail", {
+test_that("OTP1 basic query no detail", {
   skip_if_no_otp()
   response <-
     otp_get_times(
@@ -43,10 +47,10 @@ test_that("basic query no detail", {
     )
   expect_equal(length(response), 3)
   expect_equal(response$errorId, "OK")
-  expect_equal(round(response$duration, 2), transit_duration)
+  expect_equal(round(response$duration, 1), transit_duration)
 })
 
-test_that("query with detail", {
+test_that("OTP1 query with detail", {
   skip_if_no_otp()
   response <-
     otp_get_times(
@@ -74,10 +78,10 @@ test_that("query with detail", {
       "transfers"
     )
   )
-  expect_equal(round(response$itineraries$duration, 2), transit_duration)
+  expect_equal(round(response$itineraries$duration, 1), transit_duration)
 })
 
-test_that("query with legs", {
+test_that("OTP1 query with legs", {
   skip_if_no_otp()
   response <-
     otp_get_times(
@@ -108,10 +112,10 @@ test_that("query with legs", {
       "legs"
     )
   )
-  expect_equal(round(response$itineraries[1, "duration"], 2), transit_duration)
+  expect_equal(round(response$itineraries[1, "duration"], 1), transit_duration)
 })
 
-test_that("query with arriveby", {
+test_that("OTP1 query with arriveby", {
   skip_if_no_otp()
   response <-
     otp_get_times(
@@ -125,27 +129,194 @@ test_that("query with arriveby", {
       includeLegs = TRUE,
       arriveBy =  TRUE
     )
-  expect_equal(response$itineraries[1, "end"], arriveBy_time)
+  expect_equal(round(response$itineraries[1, "end"], "mins"), arriveBy_time)
 })
 
-test_that("all parameters are passed in query", {
+test_that("OTP1 all parameters are passed in query", {
   skip_if_no_otp()
   response <-
-    otp_get_times(
+    suppressWarnings(otp_get_times(
       otpcon,
       fromPlace = fromPlace,
       toPlace = toPlace,
       date = date,
       time = time,
       mode = "TRANSIT",
-      maxWalkDistance = 800,
-      walkReluctance = 2,
-      waitReluctance = 1,
-      arriveBy = FALSE,
-      transferPenalty = 0,
+      maxWalkDistance = 1600,
+      walkReluctance = 4,
+      waitReluctance = 2,
+      arriveBy = TRUE,
+      transferPenalty = 10,
       minTransferTime = 600,
       detail = TRUE,
-      includeLegs = TRUE
-    )
+      includeLegs = TRUE,
+      extra.params = list(optimize = "TRANSFERS")
+    ))
   expect_equal(response$query, response_query)
+})
+
+test_that("OTP1 warning on use of extra.params", {
+  skip_if_no_otp()
+  expect_warning(otp_get_times(otpcon,
+                               fromPlace = fromPlace,
+                               toPlace = toPlace,
+                               maxWalkDistance = 80000,
+                               extra.params = list("FOO" = 1)), "Unknown parameters were passed to the OTP API without checks: FOO")
+})
+
+# OTP2
+
+test_that("OTP2 Check for invalid mode", {
+  skip_if_no_otp()
+  error <-
+    try(otp_get_times(otpcon_v2,
+                      fromPlace = fromPlace,
+                      toPlace = toPlace,
+                      mode = "FOO"),
+        silent = TRUE)
+  expect_equal(grepl("Mode must be one of:", error, fixed = TRUE), TRUE)
+})
+
+test_that("OTP2 basic query no detail", {
+  skip_if_no_otp()
+  response <-
+    otp_get_times(
+      otpcon_v2,
+      fromPlace = fromPlace,
+      toPlace = toPlace,
+      date = date,
+      time = time,
+      mode = "TRANSIT",
+      maxWalkDistance = 800
+    )
+  expect_equal(length(response), 3)
+  expect_equal(response$errorId, "OK")
+  expect_equal(round(response$duration, 1), transit_duration)
+})
+
+test_that("OTP2 query with detail", {
+  skip_if_no_otp()
+  response <-
+    otp_get_times(
+      otpcon_v2,
+      fromPlace = fromPlace,
+      toPlace = toPlace,
+      date = date,
+      time = time,
+      mode = "TRANSIT",
+      detail = TRUE,
+      maxWalkDistance = 800
+    )
+  expect_equal(length(response), 3)
+  expect_equal(response$errorId, "OK")
+  expect_s3_class(response$itineraries, "data.frame")
+  expect_named(
+    response$itineraries,
+    c(
+      "start",
+      "end",
+      "timeZone",
+      "duration",
+      "walkTime",
+      "transitTime",
+      "waitingTime",
+      "transfers"
+    )
+  )
+  expect_equal(round(response$itineraries$duration, 1), transit_duration)
+})
+
+test_that("OTP2 query with legs", {
+  skip_if_no_otp()
+  response <-
+    otp_get_times(
+      otpcon_v2,
+      fromPlace = fromPlace,
+      toPlace = toPlace,
+      date = date,
+      time = time,
+      mode = "TRANSIT",
+      detail = TRUE,
+      includeLegs = TRUE,
+      maxWalkDistance = 800
+    )
+  expect_equal(length(response), 3)
+  expect_equal(response$errorId, "OK")
+  expect_s3_class(response$itineraries$legs[[1]], "data.frame")
+  expect_equal(nrow(response$itineraries$legs[[1]]), legs_number)
+  expect_named(
+    response$itineraries[1, ],
+    c(
+      "start",
+      "end",
+      "timeZone",
+      "duration",
+      "walkTime",
+      "transitTime",
+      "waitingTime",
+      "transfers",
+      "legs"
+    )
+  )
+  expect_equal(round(response$itineraries[1, "duration"], 1), transit_duration)
+})
+
+test_that("OTP2 query with arriveby", {
+  skip_if_no_otp()
+  response <-
+    otp_get_times(
+      otpcon_v2,
+      fromPlace = fromPlace,
+      toPlace = toPlace,
+      date = date,
+      time = time,
+      mode = "TRANSIT",
+      detail = TRUE,
+      includeLegs = TRUE,
+      arriveBy =  TRUE,
+      maxWalkDistance = 800
+    )
+  expect_equal(round(response$itineraries[1, "end"], "mins"), arriveBy_time)
+})
+
+test_that("OTP2 all parameters are passed in query", {
+  skip_if_no_otp()
+  response <-
+    suppressWarnings(otp_get_times(
+      otpcon_v2,
+      fromPlace = fromPlace,
+      toPlace = toPlace,
+      date = date,
+      time = time,
+      mode = "TRANSIT",
+      maxWalkDistance = 1600,
+      walkReluctance = 4,
+      waitReluctance = 2,
+      arriveBy = TRUE,
+      transferPenalty = 10,
+      minTransferTime = 600,
+      detail = TRUE,
+      includeLegs = TRUE,
+      extra.params = list(optimize = "TRANSFERS")
+    ))
+  expect_equal(response$query, response_query_otp2)
+})
+
+test_that("OTP2 warning on use of extra.params", {
+  skip_if_no_otp()
+  expect_warning(otp_get_times(otpcon_v2,
+                      fromPlace = fromPlace,
+                      toPlace = toPlace,
+                      maxWalkDistance = 0,
+                      extra.params = list("FOO" = 1)), "Unknown parameters were passed to the OTP API without checks: FOO")
+})
+
+test_that("OTP2 empty itinerary is intercepted", {
+  skip_if_no_otp()
+  response <- otp_get_times(otpcon_v2,
+                            fromPlace = fromPlace,
+                            toPlace = toPlace,
+                            maxWalkDistance = 800)
+  expect_equal(response$errorId, -9999)
+  expect_equal(grepl("No itinerary returned", response$errorMessage, fixed = TRUE), TRUE)
 })
